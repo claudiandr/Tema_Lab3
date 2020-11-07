d6d0d48c968991e916b2c4e93fb0271eb10d5997
Claudia-Andrei <claudia.andrei@mta.ro>
2020-11-07 20:54:26 +0200

    Adaugare clasa Produse
    m adaugat clasa produse care contine urmatorii membri: nume_produs,
    categorie, pret, cod_bare si un constructor.
    Signed-off-by: Claudia-Andrei <claudia.andrei@mta.ro>

---
+++ src/Produse.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Produse {
+
+    String nume_produs;
+    String categorie;
+    int pret;
+    int cod_bare;
+
+
+    public Produse(String nume_produs, String categorie, int pret, int cod_bare) {
+        this.nume_produs = nume_produs;
+        this.categorie = categorie;
+        this.pret = pret;
+        this.cod_bare = cod_bare;
+    }
+
+
+}

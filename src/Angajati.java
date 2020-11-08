6620cef02660292d997488ed9ab238fe0a90cb15
Seba <sebamanea@gmail.com>
2020-11-08 17:40:56 +0200

    Adaugare clasa Angajati Am adaugat clasa Angajati care contine Nume, Email, Functie, Salariu impreuna cu constructorul
    Signed-off-by: Seba <sebamanea@gmail.com>

---
+++ src/Angajati.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Angajati {
+
+    String Nume;
+    String Email;
+    String Functie;
+    int Salariu;
+
+
+    public Angajati(String Nume, String Email, String Functie, int Salariu) {
+        this.Nume = Nume;
+        this.Email = Email;
+        this.Functie = Functie;
+        this.Salariu = Salariu;
+
+    }
+
+
+
+}

35b702cdd77985e66d072666644103fbc138bb63
Seba <sebamanea@gmail.com>
2020-11-08 17:50:48 +0200

    Am adaugat metodele changeName, changeEmail, changeFunction, changeSalary
    Signed-off-by: Seba <sebamanea@gmail.com>

--- src/Angajati.java
+++ src/Angajati.java
@@ -19,6 +19,20 @@ public class Angajati {
     public String getInfo() {
         return "Angajatul" + this.Nume + " are functia " + this.Functie + "si salariul de " + this.Salariu + "lei";
     }
+    
+    public void changeName(String name) {
+        this.Nume = name;
+
+    }
+    public void changeEmail(String email) {
+        this.Email=email;
+    }
+    public void changeFunction(String Function) {
+        this.Functie=Function;
+    }
+    public void changeSalary(int salary) {
+        this.Salariu=salary;
+    }
 
 
 

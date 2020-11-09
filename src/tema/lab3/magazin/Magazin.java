49fcc24b06c6a393bef81ce5599b37f25c958989
Bogdan <bogdan.gutu@mta.ro>
2020-11-09 20:18:41 +0200

    Am adaugat clasa Magazin
    Aceasta clasa contine 2 liste
    
    Signed-off-by: Bogdan <bogdan.gutu@mta.ro>

---
+++ src/tema/lab3/magazin/Magazin.java
@@ -0,0 +1,13 @@
+package tema.lab3.magazin;
+
+import java.util.ArrayList;
+
+public class Magazin {
+    private ArrayList<Angajati> listaAngajati;
+    private ArrayList<Produse> listaProduse;
+    private String numeMagazin;
+
+    public Magazin() {
+        listaAngajati=new ArrayList<Angajati>();
+    }
+}
---
+++ src/tema/lab3/magazin/Main.java
@@ -0,0 +1,4 @@
+package tema.lab3.magazin;
+
+public class Main {
+}
